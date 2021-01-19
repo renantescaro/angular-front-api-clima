166ae4aff9d7f00bd72daeb3071b77c3c3df8875
renantescaro <renantescaro@yahoo.com.br>
2021-01-19 20:55:55 -0300

    importando componente notfound e pegarLocalizacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ClimaModule } from './clima/clima.module';
 import { AppRoutingModule } from './app.routing.module';
+import { PegarLocalizacaoComponent } from './pegar-localizacao/pegar-localizacao.component';
+import { NotFoundComponent } from './errors/not-found/not-found.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NotFoundComponent,
+    PegarLocalizacaoComponent,
   ],
   imports: [
     BrowserModule,
